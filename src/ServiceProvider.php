faf9e8d29fa6713b1018720aaf84f707d1263859
Joyce <787027175@qq.com>
2024-06-16 21:56:58 +0800

    ✨(Provider):注册注解

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,15 +5,18 @@ namespace LanDao\LaravelCore;
 
 use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
 use LanDao\LaravelCore\Annotation\InteractsWithInject;
+use LanDao\LaravelCore\Annotation\InteractsWithRoute;
 
 class ServiceProvider extends LaravelServiceProvider
 {
-    use InteractsWithInject;
+    use InteractsWithInject,InteractsWithRoute;
 
     public function boot(): void
     {
         //自动注入
         $this->autoInject();
+        //路由注解
+        $this->registerRoutes();
         $this->registerNamespaces();
     }
 
