179e58d6bb4ebfeef1f96cfb43733072cb70a0fc
Joyce <787027175@qq.com>
2024-07-10 00:15:02 +0800

    ✨(Model):model相关

---
+++ src/Model/BaseModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace LanDao\LaravelCore\Model;
+
+use DateTimeInterface;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+
+abstract class BaseModel extends Model
+{
+    /**
+     * 重写日期序列化
+     * @param DateTimeInterface $date
+     * @return string
+     */
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format(Carbon::parse($date)->toDateTimeString());
+    }
+}
