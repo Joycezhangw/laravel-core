df7a909377503ea73f11bae4bcb1166f725ee617
Joyce <787027175@qq.com>
2024-07-10 00:27:58 +0800

    ✨(artisan):命令生成文件服务注册

---
+++ src/Providers/ConsoleServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace LanDao\LaravelCore\Providers;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\ServiceProvider;
+use LanDao\LaravelCore\Commands;
+
+class ConsoleServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->commands(self::defaultCommands()->toArray());
+    }
+
+    public function provides(): array
+    {
+        return self::defaultCommands()->toArray();
+    }
+
+    public static function defaultCommands(): Collection
+    {
+        return collect([
+            Commands\Make\EnumMakeCommand::class,
+            Commands\Make\RepositoryMakeCommand::class,
+            Commands\Make\ModelMakeCommand::class,
+            Commands\Make\RequestMakeCommand::class
+        ]);
+    }
+}
